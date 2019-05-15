f9fb9ba7664d104c8add46afd3ec4c8bb665b3a9
arleybri18 <arleybri18@gmail.com>
2019-05-15 18:39:09 +0000

    handle error if the file is not sended or file not exist or exist more arguments

--- main.c
+++ main.c
@@ -5,9 +5,20 @@ char *linevalue = NULL;
 
 int main(int argc, char *argv[])
 {
+	struct stat st;
 	if (argc == 2)
 	{
+		if (stat(argv[1], &st) != 0)
+		{
+			write(STDERR_FILENO,"USAGE: monty file\n", 18);
+			exit(EXIT_FAILURE);
+		}
 		read_file(argv[1]);
+	}
+	else
+	{
+		write(STDERR_FILENO,"USAGE: monty file\n", 18);
+		exit(EXIT_FAILURE);
 	}
 	return (0);
 }
