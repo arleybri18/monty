da93ff351b2655438b50436e21b08ccb1a743f10
arleybri18 <arleybri18@gmail.com>
2019-05-16 21:32:58 +0000

    change range

--- instructions_3.c
+++ instructions_3.c
@@ -74,7 +74,7 @@ void pchar_handler(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if ((*stack)->n < 0 || (*stack)->n > 128)
+	if ((*stack)->n < 0 || (*stack)->n >= 128)
 	{
 		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
