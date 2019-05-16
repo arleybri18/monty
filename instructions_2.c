8cc96c3eb3f952347597fe1323fe3015672a51a1
Jose Medina <fermed28@gmail.com>
2019-05-16 16:11:48 +0000

    [feature] add sub code

--- instructions_2.c
+++ instructions_2.c
@@ -60,3 +60,32 @@ void add_handler(stack_t **stack, unsigned int line_number)
 	aux_node->prev = NULL;
 	(*stack) = aux_node;
 }
+/**
+ *sub_handler - add the data of the top to second element
+ *
+ *@stack: head of the list
+ *@line_number: number of the line with the instruction
+ *Return: Nothing
+ */
+
+void sub_handler(stack_t **stack, unsigned int line_number)
+{
+	stack_t *aux_node = *stack;
+	int len = 0;
+
+	while (aux_node)
+	{
+		len++;
+		aux_node = aux_node->next;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	aux_node = (*stack)->next;
+	(*stack)->next->n -= (*stack)->n;
+	free(*stack);
+	aux_node->prev = NULL;
+	(*stack) = aux_node;
+}
